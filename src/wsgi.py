147039f5334b19b0dc842f08aada7d469f60e08d
Taras Gutsaliuk <gutsalyuk.taras@gmail.com>
2016-11-19 18:23:51 +0200

    wsgi changes

--- src/wsgi.py
+++ src/wsgi.py
@@ -1,16 +1,12 @@
-"""
-WSGI config for src project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
-"""
 
 import os
+import sys
+
+path = '/home/studs/src'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'src.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "src.settings")
-
 application = get_wsgi_application()
