daf2ea49fc9165eefff56f7ca8c0bceda0ee1c46
Taras Gutsaliuk <gutsalyuk.taras@gmail.com>
2016-11-22 23:52:52 +0200

    Fixed mistype in media_url

--- src/settings.py
+++ src/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-EDIA_URL = '/media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, '..', 'media')
 
 
