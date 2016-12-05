cb3a094285632a23789d11b003a897ea77557ff4
Taras Gutsaliuk <gutsalyuk.taras@gmail.com>
2016-12-06 01:42:39 +0200

    Moved to new maindb2

--- src/settings.py
+++ src/settings.py
@@ -79,22 +79,22 @@ WSGI_APPLICATION = 'src.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, '..', 'db.sqlite3'),
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'studs$maindb2',
-#         'USER': 'studs',
-#         'PASSWORD': '1qaz0okm_SQL',
-#         'HOST': 'studs.mysql.pythonanywhere-services.com',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, '..', 'db.sqlite3'),
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'studs$maindb2',
+        'USER': 'studs',
+        'PASSWORD': '1qaz0okm_SQL',
+        'HOST': 'studs.mysql.pythonanywhere-services.com',
+    }
+}
 
 
 # Password validation
