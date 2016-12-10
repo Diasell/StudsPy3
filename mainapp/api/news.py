4128d1572a22e2d0658055ca286c6577652a63b6
Taras Gutsaliuk <gutsalyuk.taras@gmail.com>
2016-12-10 13:44:00 +0200

    Added LikeNewsView

--- mainapp/api/news.py
+++ mainapp/api/news.py
@@ -19,10 +19,15 @@ from rest_framework.generics import ListAPIView
 from mainapp.serializers.serializer import (
     NewsListSerializer,
     NewsContentSerializer)
-from mainapp.serializers.docs_serializer import NewsContentSerializer as d_content
+from mainapp.serializers.docs_serializer import (
+    NewsContentSerializer as d_content,
+
+)
 
 # import needed app models
-from mainapp.models.news import NewsItemModel
+from mainapp.models.news import (
+    NewsItemModel,
+    LikeNewsModel)
 
 # import my own helper methods
 from ..utils.custom_utils import *
@@ -49,7 +54,6 @@ class NewsContentView(APIView):
 
 
 
-
 class NewsListView(ListAPIView):
     authentication_classes = (IsAuthenticated,)
     pagination_class = PageNumberTPagination
@@ -58,3 +62,36 @@ class NewsListView(ListAPIView):
     def get_queryset(self, *args, **kwargs):
         queryset = NewsItemModel.objects.all().order_by('-created')
         return queryset
+
+
+class LikeNewsView(APIView):
+    authentication_classes = (IsAuthenticated,)
+    serializer_class = LikeNewsSerializer
+
+    def post(self, request):
+        user = request.user
+        news_id = request.data['id']
+        value = request.data['value']
+        item = NewsItemModel.objects.filter(id=news_id)
+        if item:
+            like, created = LikeNewsModel.objects.get_or_create(
+                user=user,
+                news=item[0],
+                value=int(value)
+            )
+            serializer = NewsListSerializer(item[0])
+
+            if created:
+                like.save()
+                response = create_response_scelet('success', 'got it', serializer.data)
+                return Response(response, status=status.HTTP_201_CREATED)
+            if not created:
+                like.value = int(value)
+                like.save()
+                response = create_response_scelet('success', 'changed', serializer.data)
+                return Response(response, status=status.HTTP_200_OK)
+        else:
+            response = create_response_scelet('failed', 'ID not found', {})
+            return Response(response, status=status.HTTP_404_NOT_FOUND)
+
+
